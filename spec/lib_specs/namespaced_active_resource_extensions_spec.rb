5772b8c2834451a1d775e0903f1895b20c5226db
Felipe Kaufmann <felipekaufmann@gmail.com>
2010-09-14 15:04:11 +0200

    added spec for namespaced resource

---
+++ spec/lib_specs/namespaced_active_resource_extensions_spec.rb
@@ -0,0 +1,107 @@
+require 'spec_helper'
+
+module Ns
+  class Ns::Customer < ActiveResource::Base
+    self.site = ''
+  end
+end
+
+describe Ns::Customer do
+  before(:each) do
+    Dupe.reset
+  end
+  
+  it "should have none url patterns registered" do
+    [:post, :get, :delete, :put].each do |verb|
+      Dupe.network.mocks[verb].should be_blank
+    end
+  end
+  
+  
+  context "registerd" do
+    before(:each) do
+      Dupe.create Ns::Customer, :name => 'Ribi', :state => 'active'
+    end
+  
+    it "should be registered as get mock" do
+      Dupe.network.mocks[:get].size == 2
+    end
+  
+    it "should generate a GetMock" do
+      customer = Dupe.find :"B2b::Customer"
+      customer.should_not be_nil
+      customer.class.should == Dupe::Database::Record
+    end
+    
+    it "should generate a valid url pattern" do
+      Dupe.network.mocks[:get].each_with_index do |mock, index|
+        mock.url_pattern.class.should == Regexp
+        if index == 0
+          mock.url_pattern.inspect.should == (%r{^\/customers\.xml$}).inspect
+        else
+          mock.url_pattern.inspect.should == %r{^\/customers\/(\d+)\.xml$}.inspect
+        end      
+      end
+    end
+    
+    
+    context "find single resource" do
+      before(:each) { @customer = Ns::Customer.find 1 }
+      
+      it "should find customer with id 1" do
+        @customer.should_not be_blank
+      end
+    end
+    
+    
+    context "find every resource" do
+      before(:each) { @customers = Ns::Customer.find :all }
+      
+      it "should return an array" do
+        @customers.class.should == Array
+      end
+      
+      it "should include customer number one" do
+        @customers.should be_include(Ns::Customer.find(1))
+      end
+      
+      it "should have size of one" do
+        @customers.size.should == 1
+      end
+    end
+    
+    
+    context "find every resource with a param-filter" do
+      before(:each) do
+        Dupe.create Ns::Customer, :name => 'Marco', :state => 'inactive'
+        Get %r{\/customers\.xml\?state=(active|inactive)} do |state|
+          if state == 'active'
+            Dupe.find(:"Ns::Customers") {|customer| customer.state == 'active'}
+          elsif state == 'inactive'
+            Dupe.find(:"Ns::Customers") {|customer| customer.state == 'inactive'}
+          else
+            Dupe.find(:"Ns::Customers")
+          end
+        end
+      end
+      
+      it "should find all active customers" do
+        active_customers = Ns::Customer.find :all, :params => {:state => 'active'}
+        active_customers.size.should == 1
+        active_customers.should be_include(Customer.find(1))
+      end
+      
+      it "should find all inactive customers" do
+        inactive_customers = Ns::Customer.find :all, :params => {:state => 'inactive'}
+        inactive_customers.size.should == 1
+        inactive_customers.should be_include(Ns::Customer.find(2))
+      end
+      
+      it "should find all customers without a filter" do
+        customers = Ns::Customer.find :all, :params => {}
+        customers.size.should == 2
+        customers.should == Ns::Customer.find(:all)
+      end
+    end
+  end
+end
